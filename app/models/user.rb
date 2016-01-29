9a14fce073f1df50da499856c2e14b447d23b3ce
Steven Cassidy <steven.cassidy@gmail.com>
2016-01-28 21:07:26 -0500

    Rename associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,19 @@ class User < ActiveRecord::Base
 
   has_many :items
   has_many :given_swipes, through: :items
-  has_many :bid_items_swiped, through: :given_swipes, source: :judged_item
-  has_many :offered_items_swiped, through: :given_swipes, source: :owned_item
 
-  has_many :bid_item_matches, through: :bid_items_swiped, source: :accepted_matches
-  has_many :offered_item_matches, through: :offered_items_swiped, source: :proposed_matches
+  has_many :bid_swipe_items, through: :given_swipes, source: :judged_item
+  has_many :offered_swipe_items, through: :given_swipes, source: :owned_item
+
+  has_many :bid_item_matches, through: :bid_swipe_items, source: :accepted_matches
+  has_many :offered_item_matches, through: :offered_swipe_items, source: :proposed_matches
 
   def wanted_items
-    bid_items_swiped.where('swipes.direction_like = ?', true)
+    bid_swipe_items.where('swipes.direction_like = ?', true)
   end
 
   def rejected_items
-    bid_items_swiped.where('swipes.direction_like = ?', false)
+    bid_swipe_items.where('swipes.direction_like = ?', false)
   end
 
 end
