b23975b629e2ac15cdbc3f9c8df3d93b39dddd67
Steven Cassidy <steven.cassidy@gmail.com>
2016-01-28 20:54:38 -0500

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,16 @@
 u1 = User.create!(name:'steven', email:'steven@example.com', password:'1234567')
 u2 = User.create!(name:'jim', email:'jim@example.com', password:'1234567')
 
-i11 = u1.items.create!(name: 'AAAAA')
-i12 = u1.items.create!(name: 'BBBBB')
-i13 = u1.items.create!(name: 'CCCCC')
+i1 = u1.items.create!(name: 'AAAAA')
+i2 = u1.items.create!(name: 'BBBBB')
+i3 = u1.items.create!(name: 'CCCCC')
 
-i21 = u2.items.create!(name: 'XXXXX')
-i22 = u2.items.create!(name: 'YYYYY')
-i23 = u2.items.create!(name: 'ZZZZZ')
+i4 = u2.items.create!(name: 'XXXXX')
+i5 = u2.items.create!(name: 'YYYYY')
+i6 = u2.items.create!(name: 'ZZZZZ')
 
-i11.given_swipes.create(judged_item: i22, direction_like: true)
-i22.given_swipes.create(judged_item: i11, direction_like: true)
-i11.given_swipes.create(judged_item: i23, direction_like: true)
-i22.given_swipes.create(judged_item: i13, direction_like: false)
-
+i1.given_swipes.create(judged_item: i5, direction_like: true)
+i5.given_swipes.create(judged_item: i1, direction_like: true)
+i5.given_swipes.create(judged_item: i2, direction_like: true)
+i2.given_swipes.create(judged_item: i6, direction_like: false)
+i3.given_swipes.create(judged_item: i4, direction_like: true)
