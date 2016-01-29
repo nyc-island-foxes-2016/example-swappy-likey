11e734cc209c97e57ece7f0513e38a480e457920
Steven Cassidy <steven.cassidy@gmail.com>
2016-01-28 20:41:03 -0500

    Add some more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(name:'steven', email:'steven@example.com', password:'1234567')
+u1 = User.create!(name:'steven', email:'steven@example.com', password:'1234567')
+u2 = User.create!(name:'jim', email:'jim@example.com', password:'1234567')
+
+i11 = u1.items.create!(name: 'AAAAA')
+i12 = u1.items.create!(name: 'BBBBB')
+i13 = u1.items.create!(name: 'CCCCC')
+
+i21 = u2.items.create!(name: 'XXXXX')
+i22 = u2.items.create!(name: 'YYYYY')
+i23 = u2.items.create!(name: 'ZZZZZ')
+
+i11.given_swipes.create(judged_item: i22, direction_like: true)
+i22.given_swipes.create(judged_item: i11, direction_like: true)
+i11.given_swipes.create(judged_item: i23, direction_like: true)
+i22.given_swipes.create(judged_item: i13, direction_like: false)
+
